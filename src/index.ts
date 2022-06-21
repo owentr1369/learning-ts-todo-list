0cfdda9d2fa49c21ed889ec1e65d25a296bbfae4
TranTam <tamvaa1306@gmail.com>
2022-06-21 11:19:37 +0700

    saveTask to LS

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,54 @@
+import { check } from 'prettier';
 import { v4 as uuidV4 } from 'uuid';
 
-import { doc } from 'prettier';
-
-// console.log(uuidV4());
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
 const list = document.querySelector<HTMLUListElement>('#list');
 const form =
   (document.getElementById('new-task-form') as HTMLFormElement) || null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
+const tasks: Task[] = [];
+
 form?.addEventListener('submit', (e) => {
   e.preventDefault();
   if (input?.value === '' || input?.value == null) return;
 
-  const newTask = {
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date(),
   };
+
+  tasks.push(newTask);
+
   addListItem(newTask);
+  input.value = '';
+  console.log('tasks :>> ', tasks);
 });
-function addListItem(task: {
-  id: string;
-  title: string;
-  completed: boolean;
-  createdAt: Date;
-}) {}
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  const lable = document.createElement('label');
+  const checkbox = document.createElement('input');
+
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTask();
+  });
+
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  lable.append(checkbox, task.title);
+  item.append(lable);
+  list?.append(item);
+}
+function saveTask() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
