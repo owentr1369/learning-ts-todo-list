7465ea194533932d5c86769bac994881c35452d2
TranTam <tamvaa1306@gmail.com>
2022-06-21 10:49:02 +0700

    newTask type and addListItem

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
 import { v4 as uuidV4 } from 'uuid';
 
-console.log(uuidV4());
+import { doc } from 'prettier';
+
+// console.log(uuidV4());
+
+const list = document.querySelector<HTMLUListElement>('#list');
+const form =
+  (document.getElementById('new-task-form') as HTMLFormElement) || null;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input?.value === '' || input?.value == null) return;
+
+  const newTask = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  addListItem(newTask);
+});
+function addListItem(task: {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+}) {}
